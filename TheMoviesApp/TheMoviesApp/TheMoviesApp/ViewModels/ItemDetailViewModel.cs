241e98c2ea49c2d56305b9862b666bef2ea3766f
ambsenyestudi <adrifre7@hotmail.com>
2019-12-15 12:10:15 +0100

    xamarin project added

---
+++ TheMoviesApp/TheMoviesApp/TheMoviesApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TheMoviesApp.Models;
+
+namespace TheMoviesApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
